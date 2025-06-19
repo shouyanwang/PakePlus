d19c969051cbf17011eafad675e49b41c48759c3
shouyanwang <136179685+shouyanwang@users.noreply.github.com>
2025-06-19 18:36:55 +0800

    update src file from pakeplus

---
+++ src/static/js/elder-DaZm3efB.js
@@ -0,0 +1,1 @@
+import{aj as e}from"./index-BCPe6f7k.js";const r=e("elder",{state:()=>({status:0,elder:{}}),actions:{setStatus(t){this.status=t},setElder(t){this.elder=t}},persist:!0});export{r as u};
